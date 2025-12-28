d33bdb7737f8551c8ec53f433039e83b873d1cb5
Chaeeun <chaeeun030311@gmail.com>
2025-12-28 17:54:14 +0900

    vpc-subnet 모듈 병합

--- modules/terraform-aws-vpc/main.tf
+++ modules/terraform-aws-vpc/main.tf
@@ -9,3 +9,38 @@ resource "aws_vpc" "campushub" {
     "karpenter.sh/discovery" = var.cluster_name
   }
 }
+
+# Subnets
+resource "aws_subnet" "public" {
+  for_each = {
+    for idx, item in var.public_subnet_config : idx => item
+  }
+
+  vpc_id                  = aws_vpc.campushub.id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.az
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name_prefix}-public-${each.key}"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_subnet" "private" {
+  for_each = {
+    for idx, item in var.private_subnet_config : idx => item
+  }
+
+  vpc_id            = aws_vpc.campushub.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.az
+
+  tags = {
+    Name = "${var.name_prefix}-private-${each.key}"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb" = "1"
+    "karpenter.sh/discovery" = var.cluster_name
+  }
+}
--- modules/terraform-aws-vpc/var.tf
+++ modules/terraform-aws-vpc/var.tf
@@ -13,3 +13,26 @@ variable "cluster_name" {
   type        = string
 }
 
+variable "availability_zones" {
+  description = "List of AZs (used for subnet grouping by index)"
+  type        = list(string)
+}
+
+variable "public_subnet_config" {
+  type = map(object({
+    cidr_block = string
+    az         = string
+  }))
+}
+
+variable "private_subnet_config" {
+  type = map(object({
+    cidr_block = string
+    az         = string
+  }))
+}
+
+variable "cluster_name" {
+  description = "EKS 클러스터 이름"
+  type        = string
+}
